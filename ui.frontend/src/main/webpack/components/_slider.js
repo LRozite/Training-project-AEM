c85a9f77b658851a5af2670cc680f4b51a07eb87
LRozite <100557449+LRozite@users.noreply.github.com>
2022-07-21 14:40:58 +0300

    21.07.2022. Before dded to my profile

--- ui.frontend/src/main/webpack/components/_slider.js
+++ ui.frontend/src/main/webpack/components/_slider.js
@@ -4,7 +4,7 @@
     
     var slider = document.getElementById("slRange");
     var output = document.getElementById("rangevalue");
-    output.innerHTML = "$" + slider.value; 
+    output.innerHTML = "$" + Number(slider.value).toFixed(2); 
     
     // Update the current slider value 
     slider.oninput = function() {
